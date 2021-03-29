3223922913caeb53b58cd7e4301768ade8331f59
MicMokum <80250147+MicMokum@users.noreply.github.com>
2021-03-29 14:09:27 +0200

    Add files via upload

---
+++ Opdracht 025 - for loops/multiplication-tables.js
@@ -0,0 +1,10 @@
+for (i = 1; i <= 10; i++) {
+    console.log(i, "keer 9 is", i * 9);
+};
+console.log("===========");
+
+for (x = 1; x <= 10; x++) {
+    for (y = 1; y <= 10; y++) {
+        console.log(y, "keer", x, "is", x * y);
+    };
+};
